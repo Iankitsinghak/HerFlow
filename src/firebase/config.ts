1fb2461c413322a4b76f5ebdaa672a42df4389df
Ankit Singh <clouds.of.ankit@gmail.com>
2025-11-26 11:04:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6892491068-beba3",
+  "appId": "1:785503461022:web:b1151e5bbc0cee7812258f",
+  "apiKey": "AIzaSyDgZbhG_QdHAuC3x6Is4nFSFtn66dQkA0g",
+  "authDomain": "studio-6892491068-beba3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "785503461022"
+};
